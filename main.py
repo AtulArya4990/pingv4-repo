3b8e6e924265a443be54e1e727c0857b80dcbb96
Daksh Jain <jaindaksh006@gmail.com>
2026-02-06 11:23:34 +0530

    Refactor test print statements in main.py

--- main.py
+++ main.py
@@ -18,34 +18,28 @@ def main():
   print("=" * 50)
   print()
 
-  # Test 1: Human vs Your Bot (You are playing as Red)
-  print("Test: Human (Red) vs Your Bot (Yellow)")
+  # Test 1: Human vs Your Bot
+  print("Test: Human vs Your Bot")
   input("Press Enter to start")
   game = Connect4Game(player1=None, player2=Bot)
   game.run()
 
-  # Uncomment additional test
-
-  # Test 2: Your Bot vs Human (Bot is playind as Red)
-  # print("Test: Your Bot (Red) vs Human (Yellow)")
-  # input("Press Enter to start")
-  # game = Connect4Game(player1=Bot, player2=None)
-  # game.run()
+  # Uncomment additional tests
 
   # Test 3: Your Bot vs Random Bot
-  # print("Test: Your Bot (Red) vs Random Bot (Yellow)")
+  # print("Test: Your Bot vs Random Bot")
   # input("Press Enter to start")
   # game = Connect4Game(player1=Bot, player2=RandomBot")
   # game.run()
 
   # Test 34: Your Bot vs Minimax Bot
-  # print("Test: Your Bot (Red) vs Minimax Bot (Yellow)")
+  # print("Test: Your Bot vs Minimax Bot")
   # input("Press Enter to start")
   # game = Connect4Game(player1=Bot, player2=MinimaxBot")
   # game.run()
 
   # Test 3: Your Bot vs Your Bot
-  # print("Test: Your Bot (Red) vs Your Bot (Yellow)")
+  # print("Test: Your Bot vs Your Bot")
   # input("Press Enter to start")
   # game = Connect4Game(player1=Bot, player2=Bot")
   # game.run()
